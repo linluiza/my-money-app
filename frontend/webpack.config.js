67095146898dfd7e40e0db862f73976480fc3498
Victor Hugo Rebelo Rodrigues <victor-hugo.rodrigues@hp.com>
2019-08-04 17:34:32 -0300

    corrigindo configuracao arquivos. adicionando arquivos principais projeto. COM ERRO em npm run dev

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -19,12 +19,30 @@ module.exports = {
             bootstrap: 'modules/admin-lte/bootstrapt/js/bootstrap.js'
         }
     },
-    plugins: {
+    plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
         new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/',
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        },{
+            test: /.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        },{
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
+        }]
     }
+
 }
