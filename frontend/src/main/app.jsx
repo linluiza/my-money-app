ccbd8970c6c96e85ae8ef77c8d3a37cf5f1b1f87
aline.luiza <aline.luiza@dataprev.gov.br>
2019-08-05 15:00:22 -0300

    adicionando footer e componente de rotas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,17 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+
+import Routes from '../main/routes'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )
