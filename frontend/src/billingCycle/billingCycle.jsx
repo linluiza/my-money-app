8514221ddb6777e182bb8ec0f61db6f02a09dc42
aline.luiza <aline.luiza@dataprev.gov.br>
2019-08-05 17:38:27 -0300

    construindo componentes tela dashboard

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
 export default props => (
     <div>
-        <h1>Ciclos de Pagamento</h1>
+        <ContentHeader title="Ciclos de Pagamento" />
+        <Content>
+            Conteudo
+        </Content>
     </div>
 )
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Row from '../common/layout/row'
+import ValueBox from '../common/widget/valueBox'
+
+const urlBackend = 'http://localhost:3003'
+
+class Dashboard extends Component{
+
+    render () {
+        return (
+        <div>
+            <ContentHeader title="Dashboard" subtitle="versao 1.0" />
+            <Content>
+                <Row>
+                    <ValueBox cols='12 4' color='green' value='R$10' text='Total de Créditos' icon='bank' />
+                    <ValueBox cols='12 4' color='red' value='R$10' text='Total de Débitos' icon='credit-card' />
+                    <ValueBox cols='12 4' color='blue' value='R$0' text='Consolidado' icon='money' />
+                </Row>
+            </Content>
+        </div>
+        )
+    }
+}
+
+export default Dashboard
