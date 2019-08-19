5f28f0354efd97185577cd0b0fbf68cd126422ab
aline.luiza <aline.luiza@dataprev.gov.br>
2019-08-19 16:22:54 -0300

    melhoria de layout do formulario

--- frontend/src/billingCycle/cycleForm.jsx
+++ frontend/src/billingCycle/cycleForm.jsx
@@ -1,15 +1,23 @@
 import React, {Component} from 'react'
 import {reduxForm, Field} from 'redux-form'
 
+import InputAndLabel from '../common/form/inputAndLabel'
+
 class CycleForm extends Component{
     render(){
         const {handleSubmit} = this.props
         return(
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field component='input' name='name'></Field>
-                    <Field component='input' name='month'></Field>
-                    <Field component='input' name='year'></Field>
+                    <Field component={InputAndLabel} name='name'
+                        label='Nome' cols='12 4'
+                        placeholder='Preencha o nome'></Field>
+                    <Field component={InputAndLabel} name='month'
+                        label='Mês' cols='12 4' type='number'
+                        placeholder='Preencha o mês'></Field>
+                    <Field component={InputAndLabel} name='year'
+                        label='Ano' cols='12 4' type='number'
+                        placeholder='Preencha o ano'></Field>
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn primary'>{this.props.buttonLabel}</button>
