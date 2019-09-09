845a7e8d062c5c1ffe6c1548ab5c31b0c265c28d
aline.luiza <aline.luiza@dataprev.gov.br>
2019-09-09 15:03:51 -0300

    atualizando versao mongoose para corrigir validacoes de subSchemas em update.

--- backend/src/api/common/errorHandler.js
+++ backend/src/api/common/errorHandler.js
@@ -2,7 +2,7 @@ const _ = require('lodash')
 
 module.exports = (request, response, next) => {
     const bundle = response.locals.bundle
-    
+
     if(bundle.errors){
         response.status(500).json(parseErrorMessages(bundle.errors))
     } else {
