fbc9117c0c5a8c00dc6c7f1172d8e3875def0ece
aline.luiza <aline.luiza@dataprev.gov.br>
2019-09-10 10:56:40 -0300

    codigo sem sucesso para permitir inserçao de ciclo sem debitos ou creditos

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -5,7 +5,7 @@ import {initialize} from 'redux-form'
 import {selectTab, showTabs} from '../common/tabs/tabActions'
 
 const URL_BASE = 'http://localhost:3003/api'
-const INITIAL_STATE = {credits : [{}], debts: [{}]}
+const INITIAL_STATE = {credits:[], debts:[]}
 
 function listCycles(){
     var  request = axios.get(`${URL_BASE}/billingCycles`)
@@ -16,30 +16,68 @@ function listCycles(){
 }
 
 function createNew(cycleFields){
+ //   tratarCamposObrigatoriosEArraysVazios(cycleFields)
     return dispatch => {
         console.log(cycleFields)
         axios.post(`${URL_BASE}/billingCycles`,cycleFields)
-            .then(resp => {
-                toastr.success('Sucesso', 'Operação realizada com sucesso')
-                dispatch(init())
-            })
-            .catch( e => {
-                e.response.data.forEach(
-                    error => toastr.error('Error', error)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso')
+            dispatch(init())
+        })
+        .catch( e => {
+            e.response.data.forEach(
+                error => toastr.error('Error', error)
                 )
             })
+        }
     }
-}
+    
+function tratarCamposObrigatoriosEArraysVazios(cycleFields){
+    let credits = cycleFields.credits
+    let debts = cycleFields.debts
 
+    console.log(cycleFields)
+    if(credits){
+        credits = credits.filter(function(credit){
+            return Object.keys(credit).length > 0
+        }).map((credit) => {
+
+            if(credit.value && !credit.name){
+                credit['name']=''
+            } 
+            if(credit.name && !credit.value){
+                credit['value']=null
+            }
+            return credit
+        })
+        console.log(credits)
+
+        if(credits.length == 0){
+            delete cycleFields.credits
+        } else{
+            cycleFields.credits = credits
+        }
+    }
+    console.log(credits)
+    // if(credits && credits.length == 1 && Object.keys(credits[0]).length == 0){
+        //     delete cycleFields.credits
+        // }
+    if(debts && debts.length == 1 && Object.keys(debts[0]).length == 0){
+        delete cycleFields.debts
+    }
+    console.log(cycleFields)
+}
+    
 function showTabContent(tabId,item){  
-    return [
-        showTabs(tabId),
-        selectTab(tabId),
+        return [
+            showTabs(tabId),
+            selectTab(tabId),
         initialize('billingCycleForm', item)
     ]
 }
 
 function edit(item){
+ //   tratarCamposObrigatoriosEArraysVazios(item)
     return dispatch => {
         console.log(item)
         axios.put(`${URL_BASE}/billingCycles/${item._id}`,item)
